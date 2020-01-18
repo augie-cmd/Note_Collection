3b6f4277fdb408985178824ecf644d756ee79ec2
augie <8711710@gmail.com>
2020-01-17 18:56:34 -0800

    DNS Server Basics

---
+++ DNS_Spoofing_Notes.sh
@@ -0,0 +1,10 @@
+# What is a DNS Server?
+# A DNS Server acts as a telephone book. A web address
+# needs to be translated into an IP address. The IP
+# address identifies where the web site requested by the
+# user is located.
+
+# Frequently visited sites are saved locally for
+# convenience.
+
+#
