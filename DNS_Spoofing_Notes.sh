de81fc98fba4137c22fbe73dd0420ae2deb94c14
augie <8711710@gmail.com>
2020-01-28 20:13:48 -0800

    DNS Server Basics

--- DNS_Spoofing_Notes.sh
+++ DNS_Spoofing_Notes.sh
@@ -2,9 +2,14 @@
 # A DNS Server acts as a telephone book. A web address
 # needs to be translated into an IP address. The IP
 # address identifies where the web site requested by the
-# user is located.
+# user is located. A DNS Server contains a list of web
+# addresses and their corresponding IP addresses.
+# DNS Servers provide a welcome alternative to
+# memorizing IP addresses.
 
 # Frequently visited sites are saved locally for
-# convenience.
+# convenience. A DNS database, or cache, resides on
+# your computer and contains information on sites
+# you visit commonly.
 
 #
